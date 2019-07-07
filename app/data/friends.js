762896163dbf4827190deb3867f103420c6fe3d2
sajad1993gh <sajad1993gh@gmail.com>
2019-07-06 21:15:57 -0400

    setting html routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,9 @@
 var friendsArray = [{
     name: "Sajad",
-    imgLink: "https://i.ibb.co/z6B0qLZ/Screen-Shot-2019-06-25-at-18-42-15.png"
+    imgLink: "https://i.ibb.co/z6B0qLZ/Screen-Shot-2019-06-25-at-18-42-15.png",
+    scores: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    ]
 }]
 
 module.exports = friendsArray;
