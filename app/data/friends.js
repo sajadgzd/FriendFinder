0c810b264255d0789fe3c018798e494d59cf8c84
sajad1993gh <sajad1993gh@gmail.com>
2019-07-08 19:08:45 -0400

    added more characters to friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,24 @@
 var friendsArray = [{
-    name: "Sajad",
-    photo: "https://i.ibb.co/z6B0qLZ/Screen-Shot-2019-06-25-at-18-42-15.png",
-    scores: [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ]
-}]
+        name: "Sajad",
+        photo: "https://i.ibb.co/z6B0qLZ/Screen-Shot-2019-06-25-at-18-42-15.png",
+        scores: [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ]
+    },
+    {
+        name: "Super Mario",
+        photo: "https://i.ibb.co/NmCrxx7/mario.jpg",
+        scores: [
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+        ]
+    },
+    {
+        name: "Spiderman",
+        photo: "https://i.ibb.co/nDdnrdP/spider.jpg",
+        scores: [
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+        ]
+    }
+]
 
 module.exports = friendsArray;
